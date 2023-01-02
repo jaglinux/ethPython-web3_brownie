e402c0876db86c2c0d934a62b478a2f101d3591e
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2023-01-02 02:51:43 -0800

    1/2: Fix bug
    1. txns.json write flag is w not a
    2. Convert data files to json

--- web3py/block.py
+++ web3py/block.py
@@ -18,47 +18,13 @@ def check_if_connected(func):
         func()
     return wrapper
 
-@check_if_connected
-def create_new_account():
-    new_acct = w3.eth.account.create()
-    print(new_acct)
-
-@check_if_connected
-def get_latest_block():
-    block = w3.eth.get_block('latest')
-    save_obj(block, "block.data", 'w')
-
-@check_if_connected
-def get_latest_block_number():
-    block = w3.eth.get_block('latest')
-    save_obj(block, "block.data", 'w')
-    print(block['number'])
-
-def get_saved_block():
-    data = read_obj("block.data")
-    if data is None:
-        print("No history")
-    else:
-        print(data)
-
-
-def get_transactions_hashes():
-    data = read_obj("block.data")
-    if data is None:
-        print("No history")
-    else:  
-        txns_hashes = data['transactions']
-        print("transaction hashes are ", txns_hashes)
-        print("Number of txn hashes are ", len(txns_hashes))
-        save_obj(txns_hashes, "txns.data", "w")
-
 def save_obj(data, file_name, flag):
     assert flag in ['w', 'a']
     data = Web3.toJSON(data)
-
+    data = json.loads(data)
     try:
-        with open(file_name, "a") as f:
-            f.write(data)
+        with open(file_name, flag) as f:
+            f.write(json.dumps(data, indent=4))
     except Exception as ex:
         print("data save failed", ex)
 
@@ -67,19 +33,46 @@ def read_obj(file_name):
 
     try:
         with open(file_name, "r") as f:
-            data = json.load(f)
+            data = json.loads(f.read())
     except Exception as ex:
         print("data read failed", ex)
     
     return data
 
 @check_if_connected
+def create_new_account():
+    new_acct = w3.eth.account.create()
+    print(new_acct)
+
+@check_if_connected
+def get_latest_block():
+    block = w3.eth.get_block('latest')
+    #block = {block['number']:block}
+    save_obj(block, "block.json", 'w')
+
+def get_saved_block():
+    data = read_obj("block.json")
+    if data is None:
+        print("No history")
+    else:
+        print(data, type(data), len(data))
+
+def get_transactions_hashes():
+    data = read_obj("block.json")
+    print(data)
+    if data is None:
+        print("No history")
+    else:  
+        txns_hashes = data['transactions']
+        print("transaction hashes are ", txns_hashes)
+        print("Number of txn hashes are ", len(txns_hashes))
+        save_obj(txns_hashes, "txns.json", "w")
+
+@check_if_connected
 def parse_txn():
-    txns_hashes = read_obj("txns.data")
+    txns_hashes = read_obj("txns.json")
     print("Number of transactions ", txns_hashes)
-    txn_file = read_obj("txn.data")
-    print("---------", txn_file, len(txn_file))
-    return
+    txn_file = read_obj("txn.json")
     if txn_file is None:
         txn_file = {}
     new_txn_file = {}
@@ -95,26 +88,23 @@ def parse_txn():
     if new_txn_file:
         txn_file.update(new_txn_file)
         print("----------SAVING")
-        save_obj(txn_file, "txn.data", 'a')
+        save_obj(txn_file, "txn.json", 'a')
 
 if __name__ == "__main__":
     print("Enter 0 to read latest block")
-    print("Enter 1 to read latest block number")
-    print("Enter 2 to read block from previous store")
-    print("Enter 3 to read transactions from saved block")
-    print("Enter 4 to parse transactions from saved block")
+    print("Enter 1 to read block from previous store")
+    print("Enter 2 to read transactions from saved block")
+    print("Enter 3 to parse transactions from saved block")
     print("Enter any other key to EXIT...")
     while True:
         in1 = int(input())
         if in1 == 0:
             get_latest_block()
         elif in1 == 1:
-            get_latest_block_number()
-        elif in1 == 2:
             get_saved_block()
-        elif in1 == 3:
+        elif in1 == 2:
             get_transactions_hashes()
-        elif in1 == 4:
+        elif in1 == 3:
             parse_txn()
         else:
             break
